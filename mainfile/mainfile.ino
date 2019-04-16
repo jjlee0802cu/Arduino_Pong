f3215ffddd5142e326c03a70bbaec42ae71fc9e4
Justin Lee <jjlee0802@gmail.com>
2019-04-16 00:46:48 -0400

    takes care of when one side gets hits twice
    added booleans

--- mainfile/mainfile.ino
+++ mainfile/mainfile.ino
@@ -1,9 +1,13 @@
 int forcePortOne = A1;
 int forcePortTwo = A2;
+boolean onePressed = false;
+boolean twoPressed = false;
+
 int ultraOne = A3;
 int ultraTwo = A4;
 //Two touch sensors connected to A1, and A2.
 //Two ultrasonic sensors connected to A3 and A4.
+
 int userScore1 = 0;
 int userScore2 = 0;
 
@@ -23,21 +27,38 @@ void loop() {
     //User 2 wins, reset and suspend activity
   }
   
-  touchVal1 = analogRead(forcePortOne);
-  touchVal2 = analogRead(forcePortTwo);
-  
-  if(touchVal1 < 10 && touchVal2 > 10){
-    //Rotate towards 1
-  } else if(touchVal1 > 10 && touchVal2 < 10){
-    //Rotate towards 2
-  }
+  int touchVal1 = analogRead(forcePortOne);
+  int touchVal2 = analogRead(forcePortTwo); 
 
-  ultraVal1 = analogRead(ultraOne);
-  ultraVal2 = analogRead(ultraTwo);
+  if(!twoPressed){
+    if(touchVal1 < 10 && touchVal2 > 10){//Force sensor 2 was pressed
+      twoPressed = true;
+      onePressed = false;
+      Serial.print("two\n");
+      //Rotate towards 1
+    }
+  }
+  
+  if(!onePressed){
+    if(touchVal1 > 10 && touchVal2 < 10){ //Force sensor 1 was pressed
+      onePressed = true;
+      twoPressed = false;
+      Serial.print("one\n");
+      //Rotate towards 2
+    }
+  }
+  
+
+  int ultraVal1 = analogRead(ultraOne);
+  int ultraVal2 = analogRead(ultraTwo);
 
   if(ultraVal1 > 0 && ultraVal2 <= 0) {
     userScore1++;
+//    onePressed = false;//need to reset the booleans if someone scored
+//    twoPressed = false;
   } else if(ultraVal1 <= 0 && ultraVal2 > 0) {
     userScore2++;
+//    onePressed = false;//need to reset the booleans if someone scored
+//    twoPressed = false;
   }
 }
