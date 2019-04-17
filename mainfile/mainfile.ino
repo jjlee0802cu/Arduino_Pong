9186d3d512ecc27499f69c1c337f40012dccd7ea
Justin Lee <jjlee0802@gmail.com>
2019-04-17 01:37:28 -0400

    uses ultrasonic sensor now

--- mainfile/mainfile.ino
+++ mainfile/mainfile.ino
@@ -2,11 +2,12 @@ int forcePortOne = A1;
 int forcePortTwo = A2;
 boolean onePressed = false;
 boolean twoPressed = false;
+int forceThresh = 10; //Decreases this to increase sensitivity
 
-int ultraOne = A3;
-int ultraTwo = A4;
-//Two touch sensors connected to A1, and A2.
-//Two ultrasonic sensors connected to A3 and A4.
+const int ultraOnePingPin = 7;
+const int ultraOneEchoPin = 6;
+
+int ultraTwo = 2;
 
 int userScore1 = 0;
 int userScore2 = 0;
@@ -15,9 +16,53 @@ void setup() {
   Serial.begin(9600);
 }
 
-
-
 void loop() {
+  
+  long duration, inches, cm;
+   pinMode(ultraOnePingPin, OUTPUT);
+   digitalWrite(ultraOnePingPin, LOW);
+   delayMicroseconds(2);
+   digitalWrite(ultraOnePingPin, HIGH);
+   delayMicroseconds(10);
+   digitalWrite(ultraOnePingPin, LOW);
+   pinMode(ultraOneEchoPin, INPUT);
+   duration = pulseIn(ultraOneEchoPin, HIGH);
+   cm = microsecondsToCentimeters(duration);
+   Serial.print(cm);
+   Serial.print("cm");
+   Serial.println();
+
+
+//  if(ultraOneSent == false){
+//    pinMode(ultraOnePingPin, OUTPUT);
+//    digitalWrite(ultraOnePingPin, LOW);
+//    digitalWrite(ultraOnePingPin, HIGH);
+//    digitalWrite(ultraOnePingPin, LOW);
+//    pinMode(ultraOneEchoPin, INPUT);
+//    ultraOneSent = true;
+//  }
+//
+//  Serial.println(digitalRead(ultraOneEchoPin));
+//  
+//  if(digitalRead(ultraOneEchoPin) == HIGH){
+//    Serial.println(1098);
+//    ultraOneSent = false;
+//  }
+
+//  if(ultraOneSent == true){
+//    echoCheck();
+//  } else{
+//    ultraOneSent = true;
+//    sonar.ping_timer(echoCheck);
+//    
+//  }
+ 
+  
+  
+//  if (millis() >= pingTimer) {   // pingSpeed milliseconds since last ping, do another ping.
+//    pingTimer += pingSpeed;      // Set the next ping time.
+//     // Send out the ping, calls "echoCheck" function every 24uS where you can check the ping status.
+//  }
 
   if(userScore1 == 10) {
     //User 1 wins, reset and suspend activity
@@ -31,35 +76,51 @@ void loop() {
   int touchVal2 = analogRead(forcePortTwo); 
 
   if(!twoPressed){
-    if(touchVal1 < 10 && touchVal2 > 10){//Force sensor 2 was pressed
+    if(touchVal1 < forceThresh && touchVal2 > forceThresh){//Force sensor 2 was pressed
       twoPressed = true;
       onePressed = false;
-      Serial.print("two\n");
+      Serial.print("two22222222222222222222222222222222222222\n");
       //Rotate towards 1
     }
   }
   
   if(!onePressed){
-    if(touchVal1 > 10 && touchVal2 < 10){ //Force sensor 1 was pressed
+    if(touchVal1 > forceThresh && touchVal2 < forceThresh){ //Force sensor 1 was pressed
       onePressed = true;
       twoPressed = false;
-      Serial.print("one\n");
+      Serial.print("one111111111111111111111111111111111111\n");
       //Rotate towards 2
     }
   }
   
 
-  int ultraVal1 = analogRead(ultraOne);
-  int ultraVal2 = analogRead(ultraTwo);
+//  if(ultraVal1 > 0 && ultraVal2 <= 0) {
+//    userScore1++;
+////    onePressed = false;//need to reset the booleans if someone scored
+////    twoPressed = false;
+////    
+//  } else if(ultraVal1 <= 0 && ultraVal2 > 0) {
+//    userScore2++;
+////    onePressed = false;//need to reset the booleans if someone scored
+////    twoPressed = false;
+//  }
 
-  if(ultraVal1 > 0 && ultraVal2 <= 0) {
-    userScore1++;
-//    onePressed = false;//need to reset the booleans if someone scored
-//    twoPressed = false;
-//    
-  } else if(ultraVal1 <= 0 && ultraVal2 > 0) {
-    userScore2++;
-//    onePressed = false;//need to reset the booleans if someone scored
-//    twoPressed = false;
-  }
+}
+
+
+//void echoCheck() { // Timer2 interrupt calls this function every 24uS where you can check the ping status.
+//  // Don't do code here!
+//  if (sonar.check_timer()) { // This is how you check to see if the ping was received.
+//    Serial.println("received.");
+//    // Here's where you can add code.
+//    Serial.print("Ping: ");
+//    Serial.print(sonar.ping_result / US_ROUNDTRIP_CM); // Ping returned, uS result in ping_result, convert to cm with US_ROUNDTRIP_CM.
+//    Serial.println("cm");
+//    ultraOneSent = false;
+//  }
+//}
+
+
+long microsecondsToCentimeters(long microseconds) {
+   return microseconds / 29 / 2;
 }
